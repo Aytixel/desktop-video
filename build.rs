6a34070fbe6d9e94e68f910100e2a14194112574
Aytixel <aytixel@gmail.com>
2021-08-26 00:55:00 +0200

    Create a boderless window under icons and draw on it an image

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() {
     windows::build! {
         Windows::Win32::UI::WindowsAndMessaging::*,
-        Windows::Win32::Foundation::*
+        Windows::Win32::Foundation::*,
+        Windows::Win32::System::LibraryLoader::GetModuleHandleA,
+        Windows::Win32::Graphics::Gdi::*,
+        Windows::Win32::UI::Controls::*
     };
 }
